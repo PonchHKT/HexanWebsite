45ab2a5a412116ac7d281a18e8347dd083c90bec
Shux_DEV <70896657+PonchHKT@users.noreply.github.com>
2022-02-24 02:49:49 +0100

    Update Responsive

--- components/Contact.js
+++ components/Contact.js
@@ -2,32 +2,6 @@ import styles from "../styles/contact.module.scss";
 import data from "../data/data.json";
 
 function Contact() {
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const request = await fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": event.target.getAttribute("name"),
-        ...name,
-      }),
-    });
-
-    const response = await request.json();
-
-    console.log(response);
-
-    console.log(event.target.getAttribute("name"));
-  };
   return (
     <div className={styles.contactContainer}>
       <div className={styles.contactTitle}>
@@ -37,15 +11,9 @@ function Contact() {
       </div>
 
       <div className={styles.formContainer}>
-        <form onSubmit={handleSubmit} name="contact" method="POST" netlify>
-          <input
-            type="email"
-            name="email"
-            value=""
-            required
-            placeholder={data.contact.input.placeholder}
-          />
-          <button type="submit">{data.contact.button.text}</button>
+        <form>
+          <input placeholder={data.contact.input.placeholder} />
+          <button>{data.contact.button.text}</button>
         </form>
       </div>
     </div>
