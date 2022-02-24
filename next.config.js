94da6580f7f590bf8fd778e5f97578544d327254
Shux_DEV <70896657+PonchHKT@users.noreply.github.com>
2022-02-24 01:20:52 +0100

    Update adding Netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] },
+    });
+    return cfg;
+  },
+};
